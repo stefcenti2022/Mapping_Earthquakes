9211731aa33582782fe36fdeb085ee42050da7e4
stefcenti2022 <110138522+stefcenti2022@users.noreply.github.com>
2022-10-29 00:39:27 -0400

    Add a legend with colors representing level of magnitude.

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -126,6 +126,43 @@ d3.json(earthquakeURL).then(function (data) {
     },
   }).addTo(earthquakes);
 
+  // Create a legend control object.
+  let legend = L.control({
+    position: 'bottomright',
+  });
+
+  // Add all the details for the legend.
+  legend.onAdd = function () {
+    let div = L.DomUtil.create('div', 'info legend');
+
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      '#98ee00',
+      '#d4ee00',
+      '#eecc00',
+      '#ee9c00',
+      '#ea822c',
+      '#ea2c2c',
+    ];
+
+    // Looping through our intervals to generate a label with
+    // a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+      console.log(colors[i]);
+      div.innerHTML +=
+        '<i style="background: ' +
+        colors[i] +
+        '"></i> ' +
+        magnitudes[i] +
+        (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  // Add the legend to the map
+  legend.addTo(map);
+
   // Turn on the earthquake overlay button
   earthquakes.addTo(map);
 });
